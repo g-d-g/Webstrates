4c6b9992f16de733bc64ee5409141f09303b963e
Kristian Antonsen <kristian@derfor.dk>
2016-10-17 14:53:56 +0200

    Fixed signaling issues and added sourcemap to scripts
    Fixed signaling issues and added sourcemap to scripts

--- static/webstrates.js
+++ static/webstrates.js
@@ -348,6 +348,15 @@ root.webstrates = (function(webstrates) {
 			var scripts = [];
 			webstrates.util.appendChildWithoutScriptExecution(targetElement,
 				jqml(doc.data, undefined, scripts));
+
+			// Add source maps to inlined scripts, so console errors will link properly.
+			scripts.forEach(function(script) {
+				if (!script.src) {
+					script.innerHTML += "\n//# sourceURL=main.js";
+				}
+			});
+
+			// And finally execute scripts.
 			webstrates.util.executeScripts(scripts, callback);
 		};
 
